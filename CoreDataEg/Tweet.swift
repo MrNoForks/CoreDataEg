035ab368d1caaf4b468d69114a5803c4bae3f837
MrNoForks <testingiosapp12345@gmail.com>
2019-06-14 19:12:00 +0530

    Initial Commit

---
+++ CoreDataEg/Tweet.swift
@@ -0,0 +1,16 @@
+//
+//  Tweet.swift
+//  CoreDataEg
+//
+//  Created by Boppo Technologies on 12/06/19.
+//  Copyright © 2019 Boppo Technologies. All rights reserved.
+//
+
+import CoreData
+
+class Tweet: NSManagedObject {
+    
+    
+
+    
+}
---
+++ CoreDataEg/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CoreDataEg
+//
+//  Created by Boppo Technologies on 11/06/19.
+//  Copyright © 2019 Boppo Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
